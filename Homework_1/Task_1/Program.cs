7554e93746e21374900d909bbde17d8b3e0b093b
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-08-28 22:01:20 +0300

    I'm work on Homework 1 Task 1 (end Task 1)

---
+++ Homework_1/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Блок (функция) запроса и ввода данных:
+
+/*
+Не люблю int почти всегда работаю со float
+довольно таки редко с double
+Перехожу на int только когда что-то округляю
+*/
+
+float DataVvod(string message)
+{
+//Console.Write(message);
+System.Console.Write(message);
+string strValue = Console.ReadLine() ?? ""; 
+//int Value = int.Parse (strValue);
+float Value = float.Parse (strValue);
+return Value;
+}
+
+//В чем разница между System.Console.Write и Console.Write - ?
+//На первый взгляд оде функции работают одинаково. 
+System.Console.Write("Данная программа принимает два числа и выдаёт, какое число большее, а какое меньше.");
+//Console.Write("Данная программа принимает два числа и выдаёт, какое число большее, а какое меньше.");
+
+//Блок ввода переменных
+float Value1 = DataVvod("Введите число №1: ");
+float Value2 = DataVvod("Введите число №2: ");
+
+//Блок логических условий
+if ( Value1 > Value2 ) {System.Console.Write($"Число {Value1} больше числа {Value2}");}
+if ( Value1 < Value2 ) {System.Console.Write($"Число {Value2} больше числа {Value1}");}
+else {System.Console.Write($"Число {Value2} равно числу {Value1}");}
+
+/* 
+или так но на мой взгляд это лишнее (есть всего три варианта и они описаны выше)
+для двух If значит если они не выполнятся выполнится else
+if ( Value1 == Value2 ) {System.Console.Write($"Число {Value2} равно числу {Value1}");}
+*/
