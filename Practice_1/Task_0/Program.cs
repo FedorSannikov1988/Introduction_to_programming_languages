55ec2196f3134a90fab807214600e81dc2ce3952
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-08-28 17:27:55 +0300

    I'm start work

---
+++ Practice_1/Task_0/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задание номер № 0
+//Напишите программу, которая на вход принимает 
+//число и выдаёт его квадрат (число умноженное на само себя).
+
+System.Console.WriteLine("Введите число ");
+
+string strValue = Console.ReadLine() ?? "0";
+
+int Value = int.Parse (strValue);
+
+int Result = Value * Value;
+
+System.Console.WriteLine($"Квадрат числа {Value} равен {Result}");
