05c4270b4e198d4aabc31c570a787532094419b3
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-08-28 20:22:28 +0300

    I'm end work on lection 1

---
+++ Lection_1/Example_006/Program.cs
@@ -0,0 +1,18 @@
+﻿//блок задания переменных
+int a = 36;
+int b = 2;
+int c = 96;
+int d = 63;
+int e = 11;
+
+//блок присвоения максимума
+int max = a;
+
+//блок условия (топорно)
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine($"max = {max}");
