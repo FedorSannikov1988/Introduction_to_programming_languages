9e8515391c94a2410ed7cfec87df4cee45c9160e
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-08-30 00:48:07 +0300

    I'm work on Practice 1 Task 5 (end Task 5)

---
+++ Practice_1/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу вычисления функции: x = f(a)
+На практике определились что это будет программа/функция вычисления куба (1 : 12) !
+*/
+
+int ZaPRoS(string message, int SposViVoD)
+{
+if ( SposViVoD == 0 ){ System.Console.Write(message); }
+if ( SposViVoD == 1 ){ System.Console.WriteLine(message); }
+string strValue = Console.ReadLine() ?? ""; 
+int Value = int.Parse (strValue);
+return Value;
+}
+
+System.Console.WriteLine("Программа вычисляет куб введеного числа");
+
+int ChiSlo = ZaPRoS("Введите число: ", 0);
+
+System.Console.Write($"Куб {ChiSlo} равен ");
+System.Console.Write ($"{ChiSlo*ChiSlo*ChiSlo}");
