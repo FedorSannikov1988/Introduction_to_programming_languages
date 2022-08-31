9822ab0fe492f8cb4bb941f8ff4066bc58db004d
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-08-31 08:03:06 +0300

    I'm work on Practice 1 Task 6 and Task 7 (end Task 6 and Task 7)

---
+++ Practice_1/Task_6/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+1 -> 1 
+2 -> 0.5
+0.25 -> 4
+*/
+
+//Блок ввода (немного модернизировал/параметризировал)
+float ZaPRoS(string message, int SposViVoD)
+{
+if ( SposViVoD == 0 ){ System.Console.Write(message); }
+if ( SposViVoD == 1 ){ System.Console.WriteLine(message); }
+string strValue = Console.ReadLine() ?? ""; 
+float Value = float.Parse (strValue);
+return Value;
+}
+
+System.Console.WriteLine("Данная программа выдает обратное значение введенного числа");
+System.Console.WriteLine("0 вводить нельзя");
+System.Console.WriteLine("Пример1: 1 имеет обратное значение 1");
+System.Console.WriteLine("Пример2: 2 имеет обратное значение 0,5");
+System.Console.WriteLine("Пример3: 0,25 имеет обратное значение 4");
+
+float ChiSlO = ZaPRoS("Введите число обратное значение которого нужно получить ", 0);
+
+if ( ChiSlO == 0 )
+{
+    System.Console.WriteLine($"Вы ввели {ChiSlO} что не соответствует условию задачи"); 
+   
+}
+//---
+else
+{
+   float ObrChiSlO = 1/ChiSlO ;
+   System.Console.WriteLine($"Обратное значение {ChiSlO} равно {ObrChiSlO}");
+}
---
+++ Practice_1/Task_7/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Программа для вычисления факториала введенного числа
+*/
+
+//Блок ввода данных
+int ZaPRoS(string message, int SposViVoD)
+{
+    if (SposViVoD == 0) {System.Console.Write(message);}
+    if (SposViVoD == 1) {System.Console.WriteLine(message);}
+    string strValue = Console.ReadLine() ?? "";
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int Chislo = ZaPRoS("Введите число факториал которого нужно найти ", 0);
+//---
+if ( Chislo > 0 )
+{
+int FactChislo = 1 ;
+
+for (int i = 1; i <= Chislo; i++)
+{
+    FactChislo = FactChislo*i;
+}
+
+System.Console.WriteLine($"Факториал числа {Chislo} равен {FactChislo}");
+}
+//---
+if ( Chislo == 0 )
+{
+int FactChislo = 1 ;
+System.Console.WriteLine($"Факториал числа {Chislo} равен {FactChislo}");
+}
+//---
+if ( Chislo < 0 )
+{
+System.Console.WriteLine("Факториал числа меньше 0 не сущесвует !");
+}
