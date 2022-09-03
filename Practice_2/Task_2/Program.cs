977b548f39813c0bd12f4c3c04d5849e6d094a0f
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-09-04 01:30:17 +0300

    I'm work on Practice 2 (Task 3 end)

--- Practice_2/Task_2/Program.cs
+++ Practice_2/Task_2/Program.cs
@@ -3,11 +3,13 @@
  из отрезка [10, 99] и показывает наибольшую цифру числа.
 */
 
+//---
 int RandNumber = new Random().Next(10,100);
 int byfer1 = RandNumber;
 int byfer2 = RandNumber % 10;
 int MaX = 0;
 
+//---
 while ( byfer1 > 0 )
 {
 if( byfer2 > MaX ) {MaX = byfer2;}
