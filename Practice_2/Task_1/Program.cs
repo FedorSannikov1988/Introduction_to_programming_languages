3a95fdeac292fd0d265343bd9ea52fd04915b3ba
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-09-04 00:42:00 +0300

    I'm work on Practice 2 (Task 1 not end)

--- Practice_2/Task_1/Program.cs
+++ Practice_2/Task_1/Program.cs
@@ -1,11 +1,25 @@
 ﻿/*
-Напишите программу, которая 
-принимает на вход трёхзначное число и 
+Напишите программу, которая принимает на вход трёхзначное число и 
 на выходе показывает последнюю цифру этого числа.
 */
 
+//По факту здесь можно ввести любые символы
 System.Console.Write("Введите трехзначное число: ");
 string Value = Console.ReadLine() ?? "";
 char LastChar = Value [ Value.Length - 1 ];
 
-System.Console.Write(LastChar);
+//---
+
+System.Console.WriteLine( LastChar );
+System.Console.WriteLine( "---" );
+System.Console.WriteLine( Value[0] );
+System.Console.WriteLine( Value[1] );
+System.Console.WriteLine( Value[2] );
+
+//Вообще так делать было ненужно !!!
+
+//Пытался сказать непослушали !!!
+
+//Очень плохое решение: нет проверок и прочего !
+
+//Но зато я научилмя работать с массивом string !
---
+++ Practice_2/Task_1_V2/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает последнюю цифру этого числа.
+Пример:
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+//Стибрил из домашней работы !
+
+int ZaPRoS(string message, int SposViVoD)
+{
+    if (SposViVoD == 0) {System.Console.Write(message);}
+    if (SposViVoD == 1) {System.Console.WriteLine(message);}
+    string strValue = Console.ReadLine() ?? "";
+    bool isNumber = int.TryParse(strValue, out int Value);
+    if (isNumber) {return Value;}
+    //исключение для if
+    throw new Exception ("Введенные символы невозможно преобразовать в целое число типа int !");
+}
+//Устанавливаем правила ввода для пользователя 
+System.Console.WriteLine("Данная программа на вход принимает трёхзначное число и на выходе показывает последнюю цифру этого числа");
+System.Console.WriteLine("Нумерация цифр в числе идет слева направо.");
+
+int chislo = ZaPRoS("Пожалуйста введите трехзначное число: ", 0);
+
+// Проверяем дейсвительно мы имеем дело с 3-х значным числом
+if ( chislo > 99 && chislo < 1000 )
+{
+//Контейнер для того что бы сохранить орегинальное введеное число
+int byffer = chislo;
+
+int[] massbyffer = new int[3];
+
+for (int i = 2; i >= 0; i--)
+{
+    massbyffer [i] = byffer%10;
+    byffer = byffer/10;
+}
+System.Console.WriteLine($"Последняя цифра {massbyffer [2]} введенного Вами числа {chislo}.");
+}
+
+//---
+
+if ( chislo > 1000 )
+{
+  System.Console.WriteLine($"Введенное Вами число {chislo} четырехзначное !");
+}
+
+if ( chislo >= 0 && chislo < 99 )
+{
+  System.Console.WriteLine($"Введенное Вами число {chislo} двухзначное или меньше!");
+}
+
+if ( chislo < 0 )
+{
+  System.Console.WriteLine($"Введенное Вами число {chislo} меньше нуля");
+}
