813f149fa68cea6e8766aceab6c189cc21db94df
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-09-04 13:30:35 +0300

    I'm work on Practice 2 (Task 3, Task 4, Task 5 and create Task 6)

---
+++ Practice_2/Task_4/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Напишите программу, которая принимает на вход число
+и проверяет, кратно ли оно одновременно 7 и 23.
+Пример:
+14 -> нет
+46 -> нет
+161 -> да
+*/
+
+int ZaPRoS(string message, int SposViVoD)
+{
+    if (SposViVoD == 0)
+    {
+        System.Console.Write(message);
+    }
+    if (SposViVoD == 1)
+    {
+        System.Console.WriteLine(message);
+    }
+    string strValue = Console.ReadLine() ?? "";
+    bool isNumber = int.TryParse(strValue, out int Value);
+    if (isNumber)
+    {
+        return Value;
+    }
+    throw new Exception("Введенные символы невозможно преобразовать в целое число !");
+}
+
+int mamber1 = ZaPRoS("Введите число которое необходимо проверить на кратность 7 и 23: ", 0);
+
+if (mamber1 >= 23)
+{
+    if ((mamber1 % 23) == 0 && (mamber1 % 7) == 0)
+    {
+        System.Console.WriteLine($"Число {mamber1} кратно 7 и 23");
+    }
+    //---
+    else
+    {
+        System.Console.WriteLine($"Число {mamber1} одновременно не кратно 7 и 23");
+    }
+}
+//---
+if (mamber1 < 23)
+{
+    System.Console.WriteLine($"Введенное число {mamber1} меньше 23");
+}
---
+++ Practice_2/Task_5/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, которая будет принимать 
+на вход пять чисел и выводить сумму и среднее 
+арифметическое этих чисел.
+Пример:
+2, 5, 6, 8, 1 -> сумма 22, среднее 4,4 
+*/
+
+int ZaPRoS(string message, int SposViVoD)
+{
+    if (SposViVoD == 0) {System.Console.Write(message);}
+    if (SposViVoD == 1) {System.Console.WriteLine(message);}
+    string strValue = Console.ReadLine() ?? "";
+    bool isNumber = int.TryParse(strValue, out int Value);
+    if (isNumber) {return Value;}
+    throw new Exception ("Введенные символы невозможно преобразовать в целое число типа int !");
+}
+
+int[] massbyffer = new int[5];
+
+int length = massbyffer.Length;
+
+for (int i = 0; i < length; i++)
+{
+    massbyffer[i] = ZaPRoS($"Введите число №{i+1}: ", 0);
+}
+
+int SymmMass = 0 ;
+
+for (int i = 0; i < length; i++)
+{
+    SymmMass = SymmMass + massbyffer[i];
+}
+//---
+System.Console.WriteLine($"Сумма введенных чисел: {SymmMass}");
+System.Console.WriteLine($"Среднее арифметическое введенных чисел: {SymmMass/length}");
