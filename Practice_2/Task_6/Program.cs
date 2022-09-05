ec70147b36988cf4c25192de35ccf5d3a51198a1
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-09-05 23:02:04 +0300

    I'm work on Homework 2 Task 6 (End Task 6)

--- Practice_2/Task_6/Program.cs
+++ Practice_2/Task_6/Program.cs
@@ -1,9 +1,34 @@
 ﻿/*
-Напишите программу, которая выводит случайное число из отрезка [10, 9999] и 
+Напишите программу, которая выводит 
+случайное число из отрезка [10, 9999] и
 показывает наибольшую цифру числа.
-
 Пример:
-7812 -> 8 
-1213-> 3 
+7812 -> 8
+1213-> 3
 845 -> 8
 */
+
+int FaundMaxNamberinChislo(int Chislo)
+{
+    //контейнер для того что бы сохранить исходное число
+    int inChislo = Chislo;
+    int MaX = inChislo % 10;
+    int OsT = 0;
+
+    while (inChislo != 0)
+    {
+        inChislo = inChislo / 10;
+        OsT = inChislo % 10;
+        if (OsT > MaX)
+        {
+            MaX = OsT;
+        }
+    }
+    return MaX;
+}
+
+int RandNumber = new Random().Next(10, 10000);
+
+System.Console.WriteLine($"Случайное число: {RandNumber}");
+
+System.Console.WriteLine($"Наибольшая цифра случайного числа: {FaundMaxNamberinChislo(RandNumber)}");
