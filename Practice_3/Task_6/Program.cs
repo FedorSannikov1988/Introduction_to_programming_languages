8bae36cf24cde6d725f742e88501d7aef15fef9d
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-09-07 00:03:09 +0300

    I'm work on Practice 3 Task 5, Task 6, Task 7

--- Practice_3/Task_6/Program.cs
+++ Practice_3/Task_6/Program.cs
@@ -1,3 +1,43 @@
 ﻿/*
-Напишите программу, для вычисления факториала (через функции)
+Напишите программу, для вычисления факториала
 */
+
+int DataInput(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InputChislo()
+{
+    int chislo = DataInput("Введите число для расчета его факториала: ");
+    return chislo;
+}
+
+int Factorial(int chislo)
+{
+    int Faktorial = 1;
+
+    for (int i = 1; i <= chislo; i++)
+    {
+        Faktorial = Faktorial * i;
+    }
+    return Faktorial;
+}
+
+bool ExaminationChislo(int chislo)
+{
+    if (chislo < 0)
+    {
+        System.Console.WriteLine($"Введенное число {chislo} меньше 0");
+        return false;
+    }
+    return true;
+}
+
+int chislo = InputChislo();
+
+if (ExaminationChislo(chislo))
+{
+  System.Console.WriteLine($"{Factorial(chislo)}");
+}
--- Practice_3/Task_7/Program.cs
+++ Practice_3/Task_7/Program.cs
@@ -2,18 +2,34 @@
 Напишите программу для разворота слова (стринговой строки).
 */
 
-/*
-Взял с практики:
-
-//повернуть слово
-string word = "привет";
-char[] newWord = new char[word.Length];
-
-
-for (int i = 0; i < word.Length; i++)
+string DataInput(string message)
 {
-    newWord[i]= word[word.Length-1-i];    
+    System.Console.Write(message);
+    return (Console.ReadLine() ?? "0");
 }
 
-System.Console.WriteLine(newWord);
-*/
+string InputDataVisMessage()
+{
+    string simvol = DataInput("Введите слово/симовлы для его/их разворота (отзеркаливания): ");
+    return simvol;
+}
+
+char[] PerevertachString(string word)
+{
+    char[] newWord = new char[word.Length];
+
+    for (int i = 0; i < word.Length; i++)
+    {
+        newWord[i] = word[word.Length - (i + 1)];
+    }
+    return newWord;
+}
+
+string Word = InputDataVisMessage();
+
+System.Console.WriteLine("**********************");
+System.Console.WriteLine(Word);
+System.Console.WriteLine("**********************");
+//char и string необходимо записыват в виде приведенном ниже 
+System.Console.WriteLine(PerevertachString(Word));
+System.Console.WriteLine("**********************");
